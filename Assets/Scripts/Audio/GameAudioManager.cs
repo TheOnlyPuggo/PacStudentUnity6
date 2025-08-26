f42026d95469117fbe108eb272742fe1408b28d6
Pug <iakingdoms@gmail.com>
2025-08-26 11:09:10 +1000

    Added audio files and play on start script

---
+++ Assets/Scripts/Audio/GameAudioManager.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameAudioManager : MonoBehaviour
+{
+    [SerializeField] private AudioClip gameIntro;
+    [SerializeField] private AudioClip ghostNormalState;
+
+    private AudioSource _audioSource;
+    private bool _gameIntroPlaying = false;
+    private float _introPlayTimer = 0.0f;
+
+    private void Awake()
+    {
+        _audioSource = GetComponent<AudioSource>();
+    }
+
+    private void Start()
+    {
+        _audioSource.clip = gameIntro;
+        _audioSource.Play();
+        _gameIntroPlaying = true;
+    }
+
+    private void Update()
+    {
+        if (_gameIntroPlaying)
+        {
+            _introPlayTimer += Time.deltaTime;
+        }
+
+        if ((!_audioSource.isPlaying || _introPlayTimer >= 3.0f) && _gameIntroPlaying)
+        {
+            _gameIntroPlaying = false;
+            _audioSource.clip = ghostNormalState;
+            _audioSource.loop = true;
+            _audioSource.Play();
+        }
+    }
+}
